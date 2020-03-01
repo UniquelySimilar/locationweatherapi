d2e950ff48e38ab7de7e884f54c842f07eeaa2df
UniquelySimilar <consciousexplorer@gmail.com>
2020-03-01 11:16:34 -0700

    converted temperature to fahrenheit

--- src/main/java/com/tcoveney/locationweatherapi/controller/LocationWeatherController.java
+++ src/main/java/com/tcoveney/locationweatherapi/controller/LocationWeatherController.java
@@ -101,7 +101,8 @@ public class LocationWeatherController {
 	
 	private String retrieveWeather(RestTemplate restTemplate, String latitude, String longitude) {
 		String retVal = null;
-		String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + weatherApiKey;
+		String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude +
+				"&units=imperial&appid=" + weatherApiKey;
 		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
 		int statusCode = responseEntity.getStatusCodeValue();
 		if (statusCode != 200) {
